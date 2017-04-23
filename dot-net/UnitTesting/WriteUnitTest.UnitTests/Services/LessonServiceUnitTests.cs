9e95e9389b26a8c717d91742d719b16d3a49f21b
Joe Kehnast <joe.kehnast@verizon.net>
2017-04-23 11:54:53 -0400

    Update LessonServiceUnitTests.cs

--- dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
+++ dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WriteUnitTest.Services;
 
 namespace WriteUnitTest.UnitTests.Services
 {
@@ -8,6 +9,45 @@ namespace WriteUnitTest.UnitTests.Services
         [TestMethod]
         public void UpdateLessonGrade_Test()
         {
+            // Arrange
+            var lessonSvc = new LessonService();
+
+            // Act
+            var grade = lessonSvc._lessonRepo.GetLesson(12).Grade;
+
+            lessonSvc.UpdateLessonGrade(12, 98.2d);
+
+            // Assert
+            var gradeAfterUpdate = lessonSvc._lessonRepo.GetLesson(12).Grade;
+            Assert.AreNotEqual(grade, gradeAfterUpdate, "failed to update grade.");
+
+        }
+
+        [TestMethod]
+        public void TestGradePassed()
+        {
+            // Arrange
+            var lessonSvc = new LessonService();
+
+            // Act
+            //lessonSvc.UpdateLessonGrade(12, 98.2d);
+            lessonSvc.UpdateLessonGrade(12, 98.2d);
+
+            // Assert
+            Assert.IsTrue(lessonSvc._lessonRepo.GetLesson(12).IsPassed, "Test grade check failed for passed condition");
+        }
+
+        [TestMethod]
+        public void TestGradeFailed()
+        {
+            // Arrange
+            var lessonSvc = new LessonService();
+
+            // Act
+            lessonSvc.UpdateLessonGrade(12, 18.2d);
+
+            // Assert
+            Assert.IsFalse(lessonSvc._lessonRepo.GetLesson(12).IsPassed, "Test grade check failed for not");
         }
     }
 }
